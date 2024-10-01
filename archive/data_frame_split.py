f7739f8d4f63cfa10a87387b641bc7aa45dddcd2
dimapartov <mr_kubarev@mail.ru>
2024-10-01 19:19:12 +0300

    Refactor project structure

--- archive/data_frame_split.py
+++ archive/data_frame_split.py
@@ -1,29 +1,29 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-
-csv_file_path = '../data/data_frame_normalized.csv'
-
-# Check if encoding is needed
-data = pd.read_csv(csv_file_path, encoding='ISO-8859-1', usecols=['Hour', 'Category', 'Tyres', 'Pit Stops', 'Best Lap Kph', 'Best Lap Time', 'Status'])
-
-X = data.drop(['Best Lap Time', 'Status'], axis=1)
-Y = data[['Best Lap Time', 'Status']]
-
-X_temp, X_test, Y_temp, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
-
-X_train, X_val, Y_train, Y_val = train_test_split(X_temp, Y_temp, test_size=0.25, random_state=42)
-
-DataFrame_train = X_train.copy()
-DataFrame_train[['Best Lap Time', 'Status']] = Y_train.values
-
-DataFrame_valid = X_val.copy()
-DataFrame_valid[['Best Lap Time', 'Status']] = Y_val.values
-
-DataFrame_test = X_test.copy()
-DataFrame_test[['Best Lap Time', 'Status']] = Y_test.values
-
-# Rename files
-DataFrame_train.to_csv('../data/train.csv', index=False)
-DataFrame_valid.to_csv('../data/validation.csv', index=False)
-DataFrame_test.to_csv('../data/test.csv', index=False)
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+#
+#
+# csv_file_path = ''
+#
+# # Check if encoding is needed
+# data = pd.read_csv(csv_file_path, encoding='ISO-8859-1', usecols=['Hour', 'Category', 'Tyres', 'Pit Stops', 'Best Lap Kph', 'Best Lap Time', 'Status'])
+#
+# X = data.drop(['Best Lap Time', 'Status'], axis=1)
+# Y = data[['Best Lap Time', 'Status']]
+#
+# X_temp, X_test, Y_temp, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+#
+# X_train, X_val, Y_train, Y_val = train_test_split(X_temp, Y_temp, test_size=0.25, random_state=42)
+#
+# DataFrame_train = X_train.copy()
+# DataFrame_train[['Best Lap Time', 'Status']] = Y_train.values
+#
+# DataFrame_valid = X_val.copy()
+# DataFrame_valid[['Best Lap Time', 'Status']] = Y_val.values
+#
+# DataFrame_test = X_test.copy()
+# DataFrame_test[['Best Lap Time', 'Status']] = Y_test.values
+#
+# # Rename files
+# DataFrame_train.to_csv('', index=False)
+# DataFrame_valid.to_csv('', index=False)
+# DataFrame_test.to_csv('', index=False)
